e2e63a86a2251ad8e8539ffeb7ffc8a8d54b98e0
kekoawong <kwong6@nd.edu>
2021-06-02 15:35:50 -0400

    Added theming colors

---
+++ client/constants/theme.jsx
@@ -0,0 +1,20 @@
+import { DefaultTheme } from '@react-navigation/native';
+
+export const LightTheme = {
+    dark: false,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: "#8947ed",
+        background: 'white',
+        text: 'black'
+    },
+  };
+
+  export const DarkTheme = {
+    dark: true,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: 'rgb(255, 45, 85)',
+        background: 'white'
+    },
+  };
