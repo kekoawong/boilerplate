d3ced51feb8e09ff73bd926ce2155d681c04a403
kekoawong <kwong6@nd.edu>
2021-06-06 17:41:13 -0400

    finished initial modal

--- client/src/event/createEvent.jsx
+++ client/src/event/createEvent.jsx
@@ -1,20 +1,41 @@
-import React, { useMemo, useRef } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Modalize } from 'react-native-modalize';
+
+function modalHeader() {
+
+    return (
+        <View style={{flex: 1, width: '100%'}}>
+            <View style={styles.contentContainer}>
+                 <Text>cool cool 🎉</Text>
+            </View>
+        </View>
+    );
+}
 
 export default function CreateEvent() {
 
     // variables
     const modulize = useRef(null);
+    const navigation = useNavigation();
 
-    // callbacks
-    const handleSheetChanges = () => console.log('handle changes');
+    // open modal
+    useEffect(() => {
+        modulize.current?.open();
+    });
 
-    // renders
     return (
         <View style={{flex: 1}}>
             <Modalize
+                modalStyle={styles.container}
                 ref={modulize}
+                modalTopOffset={10}
+                onClose={() => navigation.goBack()}
+                closeOnOverlayTap={true}
+                withHandle={false}
+                HeaderComponent={modalHeader}
+                withReactModal={true}
             >
                 <View style={styles.contentContainer}>
                     <Text>Awesome 🎉</Text>
@@ -28,7 +49,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 24,
-        backgroundColor: 'transparent',
+        backgroundColor: 'white',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
